2ae189a9884d3d3f70bcfe7cb05cded9cd57d6fc
Dr. Rodrigo Segura <lolero@gmail.com>
2022-02-21 13:57:31 +0100

    build: clean up typescript config

--- packages/web/apps/web-apps-dapp-example/vite.config.ts
+++ packages/web/apps/web-apps-dapp-example/vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import reactPlugin from '@vitejs/plugin-react';
 import tsconfigPathsPlugin from 'vite-tsconfig-paths';
 
+const plugins: (PluginOption | PluginOption[])[] = [reactPlugin()];
+
+if (process.env.NODE_ENV !== 'production') {
+  plugins.push(
+    tsconfigPathsPlugin({
+      projects: ['../../../../tsconfig.json'],
+    }),
+  );
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    reactPlugin(),
-    tsconfigPathsPlugin({
-      projects: [
-        process.env.NODE_ENV === 'production'
-          ? 'tsconfig.build.json'
-          : 'tsconfig.json',
-      ],
-    }),
-  ],
+  plugins,
   build: {
     outDir: 'build',
   },
