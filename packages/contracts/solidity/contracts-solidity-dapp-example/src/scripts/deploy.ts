4412250eaa6b8b9bc595f512493a6522ecb9a7d3
Dr. Rodrigo Segura <lolero@gmail.com>
2022-02-23 12:27:43 +0100

    build: cleanup package contracts-solidity-dapp-example

--- packages/contracts/solidity/contracts-solidity-dapp-example/src/scripts/deploy.ts
+++ packages/contracts/solidity/contracts-solidity-dapp-example/src/scripts/deploy.ts
@@ -3,7 +3,7 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -14,17 +14,19 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Greeter = await ethers.getContractFactory('Greeter');
+  const greeter = await Greeter.deploy('Hello, Hardhat!');
 
   await greeter.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  // eslint-disable-next-line no-console
+  console.log('Greeter deployed to:', greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
+  // eslint-disable-next-line no-console
   console.error(error);
   process.exitCode = 1;
 });
