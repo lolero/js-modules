ed2b490d2d794797167a786a9ac9c252ce8cdaa1
Dr. Rodrigo Segura <lolero@gmail.com>
2023-02-15 16:21:27 +0100

    fix: dev and test .env config

--- packages/apps/nest-examples/apps-nest-examples-my-car-value/src/app/app.module.ts
+++ packages/apps/nest-examples/apps-nest-examples-my-car-value/src/app/app.module.ts
@@ -1,5 +1,8 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_PIPE } from '@nestjs/core';
+import cookieSession from 'cookie-session';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule, getAuthUsersServiceProvider } from '../api-nest-utils/src';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -11,17 +14,44 @@ import { UsersService } from '../users/users.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [ReportsEntity, UsersEntity],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [ReportsEntity, UsersEntity],
+          synchronize: true,
+        };
+      },
     }),
     AuthModule.register(UsersModule, getAuthUsersServiceProvider(UsersService)),
     ReportsModule,
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+    AppService,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['my car value auth cookie session key'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
